fcdb74662c8d3f1c7eebbcf36ca0a7df6aefdb48
adityaprasad-MIRAI <aditya.prasad@msot.org>
2026-09-04 12:56:55 +0530

    Adding the include folder

---
+++ include/standard.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <unordered_map>
+#include <unordered_set>
+
